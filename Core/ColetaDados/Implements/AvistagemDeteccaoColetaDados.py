db1114f77928a1dbe257dde5a681890a8a8b6eb1
JoabUrbano <joab.uaraujo@gmail.com>
2025-04-24 08:51:39 -0300

    Documentação dos métodos ColetaDados

--- Core/ColetaDados/Implements/AvistagemDeteccaoColetaDados.py
+++ Core/ColetaDados/Implements/AvistagemDeteccaoColetaDados.py
@@ -6,10 +6,29 @@ from Core.ColetaDados.Interfaces.ColetaDadosInterface import ColetaDadosInterfac
 class AvistagemDeteccaoColetaDados(ColetaDadosInterface):
 
     def __init__(self, path: str, repository):
+        """
+        Método construtor
+
+        :param path: Caminho para o arquivo a ser lido
+        :type path: str
+        :param repository: Instância de RepositoryTemplate
+        :type repository: RepositoryTemplate
+        """
         self.path = path
         self.repository = repository
 
     def carregarDados(self) -> str:
+        """
+        Método para abrir o arquivo .csv e coloca em um dataframe pandas
+
+        :return: Mensagem de sucesso ou erro.
+        :rtype: str
+
+        :raises FileNotFoundError: Se o arquivo não for encontrado no caminho recebido.
+        :raises UnicodeDecodeError: Se a codificação do arquivo for incompatível com 'utf-8'.
+        :raises pandas.errors.ParserError: Se o conteúdo do CSV estiver mal formado.
+        :raises OSError: Em caso de problemas ao acessar o arquivo.
+        """
         try:
             dados = pd.read_csv(
                 self.path, sep=",", encoding="utf-8", escapechar="\n"
@@ -17,9 +36,18 @@ class AvistagemDeteccaoColetaDados(ColetaDadosInterface):
             return self.formatarDados(dados)
         
         except Exception as e:
-           return f"Erro ao abrir o arquivo!"
+           return f"Erro ao abrir o arquivo: {e}."
 
     def formatarDados(self, dados: pd.DataFrame) -> str:
+        """
+        Método que trata os dados, realiza a limpeza e formata aos dados
+
+        :param dados: dataframe pandas que foi criado no método carregarDados
+        :type dados: pd.DataFrame
+
+        :return: Mensagem de sucesso ou erro.
+        :rtype: str
+        """
         dados["Filhotes"] = dados["Filhotes"].astype(str).replace("x", "0")
         dados["Filhotes"] = pd.to_numeric(dados["Filhotes"], errors='coerce').infer_objects(copy=False)
 
@@ -49,4 +77,13 @@ class AvistagemDeteccaoColetaDados(ColetaDadosInterface):
         return self.persistirDados(dados)
 
     def persistirDados(self, dados: pd.DataFrame) -> str:
+        """
+        Método que chama o repository para persistir os dados do dataframe
+
+        :param dados: dataframe pandas que já foi tratado no método de formatarDados
+        :type dados: pd.DataFrame
+
+        :return: Mensagem de sucesso ou erro.
+        :rtype: str
+        """
         return self.repository.inserirDados(dados)
--- Core/ColetaDados/Interfaces/ColetaDadosInterface.py
+++ Core/ColetaDados/Interfaces/ColetaDadosInterface.py
@@ -4,12 +4,30 @@ class ColetaDadosInterface:
 
     @abstractmethod
     def carregarDados(self) -> str:
+        """
+        Método para abrir o arquivo
+
+        :return: Mensagem de sucesso ou erro.
+        :rtype: str
+        """
         pass
     
     @abstractmethod
     def formatarDados(self) -> str:
+        """
+        Método que trata os dados, realiza a limpeza e formata aos dados
+
+        :return: Mensagem de sucesso ou erro.
+        :rtype: str
+        """
         pass
     
     @abstractmethod
     def persistirDados(self) -> str:
+        """
+        Método que chama o repository para persistir os dados do dataframe
+
+        :return: Mensagem de sucesso ou erro.
+        :rtype: str
+        """
         pass
