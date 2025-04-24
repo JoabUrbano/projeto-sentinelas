f4f8bdcca20b3a3e15b193921acbbce041a134f0
JoabUrbano <joab.uaraujo@gmail.com>
2025-04-24 10:27:41 -0300

    Documentação dos métodos do módulo Repository

--- Core/Repositorys/Implements/AvistagemDeteccaoRepository.py
+++ Core/Repositorys/Implements/AvistagemDeteccaoRepository.py
@@ -3,6 +3,15 @@ from Core.Repositorys.Template.RepositoryTemplate import RepositoryTemplate
 class AvistagemDeteccaoRepository(RepositoryTemplate):
 
     def inserirDados(self, dados) -> str:
+        """
+        Sobrescrição do método com os parametros do dataframe para inserir no banco
+
+        :param dados: dataframe pronto que será inserido no banco de dados
+        :type dados: pd.DataFrame
+
+        :return: Mensagem de sucesso ou erro.
+        :rtype: str
+        """
         sql = """
             INSERT INTO tb_avistagem_deteccao 
             (expedicao, numero, navio, data, registro, especie, nome_comum, latitude, longitude, filhotes, observacoes, tamanho_grupo_minimo, tamanho_grupo_maximo) 
--- Core/Repositorys/Template/RepositoryTemplate.py
+++ Core/Repositorys/Template/RepositoryTemplate.py
@@ -8,6 +8,9 @@ load_dotenv()
 class RepositoryTemplate:
 
     def __init__(self):
+        """
+        Método construtor que pega as variaveis de ambiente com as informações do banco
+        """
         self.host = os.getenv("DB_HOST")
         self.port = int(os.getenv("DB_PORT", 3306))
         self.user = os.getenv("DB_USER")
@@ -15,10 +18,37 @@ class RepositoryTemplate:
         self.database = os.getenv("DB_DATABASE")
 
     def inserirDados(self, dados: pd.DataFrame) -> str:
+        """
+        Método a ser sobrescrita com os parametros do dataframe para inserir no banco
+
+        :param dados: dataframe enviado epla camada de serviço
+        :type dados: pd.DataFrame
+
+        :return: Mensagem de sucesso ou erro.
+        :rtype: str
+        """
         sql = ""
         return self.persistirDados(dados, sql)
     
     def persistirDados(self, dados, sql) -> str:
+        """
+        Método para persistir os dados no banco
+
+        :param dados: dataframe enviado epla camada de serviço
+        :type dados: pd.DataFrame
+        :param sql: Comando SQL para inserir os dados
+        :type sql: str
+
+        :return: Mensagem de sucesso ou erro.
+        :rtype: str
+
+        :raises AttributeError: Se o objeto `dados` não for um DataFrame válido.
+        :raises ValueError: Se houver problemas na conversão dos dados para tuplas.
+        :raises pymysql.err.OperationalError: Se houver erro na conexão com o banco de dados.
+        :raises pymysql.err.ProgrammingError: Se o comando SQL estiver incorreto.
+        :raises pymysql.err.IntegrityError: Se houver violação de integridade no banco.
+        :raises pymysql.MySQLError: Para quaisquer outros erros relacionados ao MySQL.
+        """
         valores = dados.to_records(index=False).tolist()
         try:
             with pymysql.connect(
